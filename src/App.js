89eb05f7bee595941c4e1a0e9f0372716b5b18c8
Amin Addow <amin.addow.96@gmail.com>
2019-03-21 12:13:16 +0100

    init
    Oppsett er gjennomført

--- src/App.js
+++ src/App.js
@@ -1,60 +1,88 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Link, Redirect, withRouter} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import WelcomeToWorkspace from './workspace.js';
+import Login from './login.js';
 import firebase from "./firebase";
 
-const db = firebase.firestore();
+// const db = firebase.firestore();
 
-function getNew() {
-     db.collection("react")
-        .doc("fart")
-        .get()
-        .then(doc => {
-          if (!doc.exists) {
-            console.log("No such document!");
-          } else {
-            console.log("Document data:", doc.get("speed"));
-          }
-        })
-        .catch(err => {
-          console.log("Error getting document", err);
-        });
-    };
+// function getNew() {
+//      db.collection("react")
+//         .doc("fart")
+//         .get()
+//         .then(doc => {
+//           if (!doc.exists) {
+//             console.log("No such document!");
+//           } else {
+//             console.log("Document data:", doc.get("speed"));
+//           }
+//         })
+//         .catch(err => {
+//           console.log("Error getting document", err);
+//         });
+//     };
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = { speed: 10 };
-  }
-
-  componentDidMount() {
-      getNew();
-      this.setState({ speed : 13})
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/*  <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Reacten
-          </a> */}
-
-          {/* <div>Lets make this application</div> */}
-          <div>{this.state.speed}</div>
-        </header>
-      </div>
-    );
-  }
+function Index() {
+    return <h2>Home</h2>;
 }
 
-export default App;
+function Authentication() {
+    return <Login />
+}
+function Workspace() {
+    return <WelcomeToWorkspace />
+}
+
+function Omoss() {
+    return <h2>Om oss</h2>;
+}
+
+function Dokumenter() {
+    return <h2>Dokumenter</h2>;
+}
+
+function AppRouter(){
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/workspace">Workspace</Link>
+                        </li>
+                        <li>
+                            <Link to="/omoss">Om oss</Link>
+                        </li>
+                        <li>
+                            <Link to="/dokumenter">Dokumenter</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Route path="/" exact component={Index} />
+                <Route path="/workspace" exact component={Workspace} />
+                <Route path="/authentication" component={Authentication} />
+                <Route path="/omoss" component={Omoss} />
+                <Route path="/dokumenter" component={Dokumenter} />
+            </div>
+        </Router>
+    )
+}
+
+// function PrivateRoute( {component: Component, ...rest }){
+//     return (
+//         <h2>Private Rout</h2>
+//         <Route {...rest} render={props => loginStatus.loggedIn ? (
+//             <Component {...props} />
+//         ) : (
+//             <Redirect to={{pathname:"/authentication",
+//                             state: { from: props.location }
+//                         }}
+//         )}
+//     )}
+
+export default AppRouter;
