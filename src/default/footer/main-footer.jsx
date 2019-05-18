453268da562f1b4803c6244dc6462effcd0e10b0
Haseeb Rana <ranahaseeb8@gmail.com>
2019-05-18 12:32:04 +0200

    pre - fetch

--- src/default/footer/main-footer.jsx
+++ src/default/footer/main-footer.jsx
@@ -36,9 +36,8 @@ const styles = theme => ({
 
  copyright: {   // removed position absolute
     padding: 10,
-    margin: 0,
     backgroundColor: theme.palette.primary.light,
-    bottom: 0
+    
 },
   Content: {
     padding: "5px 30px"
@@ -51,31 +50,32 @@ const styles = theme => ({
 function Footer(props) {
   const { classes } = props;
   return (
-    <Grid container item xs={12} sm={12} className={classes.root} style={{border: '2px solid blue'}}>
-      <Paper className={classes.paper}  xs={12} sm={12} square="true" style={{border: '2px solid green'}}>
+    <Grid container item xs={12} sm={12} className={classes.root} >
+      <Paper className={classes.paper}  xs={12} sm={12} square="true" >
 
-          <Grid item md={12} xs={12} className={classes.item} style={{border: '2px solid red'}}>
-            <Typography className={classes.Content}>
+          <Grid item md={12} xs={12} className={classes.item} >
+            <Typography align="center"  className={classes.Content}>
             Illustrasjoner av <a className={classes.links} href="https://undraw.co/">unDraw</a>
             </Typography>
           </Grid>
 
-          <Grid item md={12} xs={12} className={classes.item} style={{border: '2px solid red'}}>
-            <Typography className={classes.Content}>
+          <Grid item md={12} xs={12} className={classes.item} >
+            <Typography align="center"  className={classes.Content}>
             Banner av <a className={classes.links} href="https://unsplash.com/photos/luoaf3UV3HE">Nick Scheerbart</a>
             </Typography>
           </Grid>
 
-          <Grid item md={12} xs={12} className={classes.item} style={{border: '2px solid red'}}>
-            <Typography className={classes.Content}>
+          <Grid item md={12} xs={12} className={classes.item} >
+            <Typography align="center"  className={classes.Content}>
             <div>Icons made by <a className={classes.links}href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a className={classes.links}href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a  className={classes.links}href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             </Typography>
           
           </Grid>
 
 
-          <Grid item md={12} xs={12} className={classes.item} style={{border: '2px solid red'}}>
-              <Typography variant="body2" className={classes.Content}>
+          <Grid item md={12} xs={12} className={classes.item} >
+              <Typography align="center"  variant="body2" className={classes.Content}>
+                
                 Markensgate 8<br />
                 Tlf: 407 01 100
                 <br /> post@minos.no
@@ -84,7 +84,7 @@ function Footer(props) {
               </Typography>
           </Grid>
 
-          <Grid item xs={12} className={classes.copyright} style={{border: '2px solid purple'}}>
+          <Grid item xs={12} align="center" className={classes.copyright} >
             <Typography variant="body2" color="secondary">2019 © Minos</Typography>
           </Grid>
 
