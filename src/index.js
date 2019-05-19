084da8a63bc1bfb43632c5713bd424b15653f762
Haseeb Rana <ranahaseeb8@gmail.com>
2019-05-19 02:15:23 +0200

    sw works, thanks to karan gupta

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./stylesheet/index.css";
 import App from "./App";
 import theme from './theme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render( 
     <MuiThemeProvider theme={theme}>
         <App />
     </MuiThemeProvider>, 
 document.getElementById("root"));
+
+serviceWorker.register();
 
