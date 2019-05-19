d611fa4fb827690c63dc66edf6aff414c40eed20
Unknown <thor-bakke@hotmail.com>
2019-05-20 01:51:35 +0200

    Minor bug fixes, removing unecessary code

--- src/default/404.jsx
+++ src/default/404.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
 
 import "./../stylesheet/404.css";
--- src/default/views/InfoCard.js
+++ src/default/views/InfoCard.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import Banner from "./Banner";
+import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
